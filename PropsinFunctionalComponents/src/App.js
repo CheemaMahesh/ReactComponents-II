9451830972a9a2918e17bfa68c3fcb61c8bc4043
Cheema Mahesh <maheshbabucheema789@gmail.com>
2023-08-03 00:17:25 +0530

    Prpos in Functional Components

---
+++ PropsinFunctionalComponents/src/App.js
@@ -0,0 +1,21 @@
+import "./styles.css";
+
+// function Card({ name, about }) {
+//   return (
+//     <div className="card">
+//       <h3>Name: {name}</h3>
+//       <span>About: {about}</span>
+//     </div>
+//   );
+// }
+
+export const Card = (name, about) => (
+  <div className="card">
+    <h3>Name: {name}</h3>
+    <span>About: {about}</span>
+  </div>
+);
+
+export default function App() {
+  return <Card name="Pranav" about="Hi, my name is Pranav Yeole." />;
+}
